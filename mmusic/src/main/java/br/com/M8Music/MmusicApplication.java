dcf971d937dc910eb466cc9a3029ce29338abfcf
Arthur <arthur.soares.nascimento31@gmail.com>
2022-11-21 16:45:49 -0300

    Criacao projeto Spring

---
+++ mmusic/src/main/java/br/com/M8Music/MmusicApplication.java
@@ -0,0 +1,13 @@
+package br.com.M8Music;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MmusicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MmusicApplication.class, args);
+	}
+
+}
---
+++ mmusic/src/test/java/br/com/M8Music/MmusicApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.M8Music;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MmusicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
